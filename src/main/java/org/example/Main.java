7ebb0d796125ffab74d7a065b6d233d3b917e72b
Chou Zijing <X00217230216@njit.edu.cn>
2024-03-06 10:02:20 +0800

    committing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,7 +50,7 @@ public class Main {
                     }
                     break;
                 case 4:
-                    System.out.println("Exiting");
+                    System.out.println("Exiting：");
                     break;
                 default:
                     System.out.println("Invalid choice");
